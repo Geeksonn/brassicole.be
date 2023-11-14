19e05181fdc4573035b21fada1ea482564904548
Olivier Sputael <sputael.o@gmail.com>
2023-11-14 17:49:30 +0100

    fixed issue with link to commande de repas ?

--- src/lib/meal.svelte
+++ src/lib/meal.svelte
@@ -2,7 +2,7 @@
 	<p class="text-xl text-brass-green mx-auto">
 		Partagez un repas convivial et profitez d'une bière offerte!
 	</p>
-	<a href="#">
+	<a class="flex justify-center mt-3" href="https://forms.gle/j2SAF4CRVkxzCX71A" target="_blank">
 		<button class="bg-brass-orange text-center">Inscrivez-vous et recevez votre bière</button>
 	</a>
 </div>
@@ -11,7 +11,7 @@
 	button {
 		@apply bg-brass-orange rounded-full;
 		@apply text-center uppercase text-white;
-		@apply px-8 py-4;
+		@apply px-8 py-4 mx-auto;
 		@apply hover:bg-brass-green hover:transition hover:ease-in hover:duration-500;
 	}
 </style>
