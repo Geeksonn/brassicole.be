b6a3bab59ac46ed8be623c8e7098157dfbc70b7f
Olivier Sputael <sputael.o@gmail.com>
2023-11-22 21:26:44 +0100

    Vertically centered

--- src/routes/wc/+page.svelte
+++ src/routes/wc/+page.svelte
@@ -2,6 +2,6 @@
 	
 </script>
 
-<div class="p-12 text-center font-title font-bold text-brass-green text-5xl uppercase">
-    <p>Nicquel</p>
+<div class="flex flex-col justify-center h-screen">
+    <p class="font-title font-bold text-6xl text-center text-brass-green">Nicquel</p>
 </div>
