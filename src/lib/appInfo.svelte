be23db9e7cbd09c6b5747fab5e6cb50a33cd5017
Olivier Sputael <sputael.o@gmail.com>
2024-06-02 12:46:08 +0200

    Acceptable V2

--- src/lib/appInfo.svelte
+++ src/lib/appInfo.svelte
@@ -1,5 +1,19 @@
-<div class="flex flex-col items-center bg-brass-green rounded-l-3xl p-3">
-    <p class="mb-5 text-background text-4xl font-title font-bold text-center">Appicole</p>
-    <img src="/image1.png" alt="Smartphone" width="200" />
-    <a target="_blank" href="https://app.brassicole.be" class="mt-5 text-brass-orange text-3xl font-semibold text-center underline">Téléchargez-la maintenant !</a>
+<div class="h-full flex flex-col items-center bg-brass-green rounded-l-3xl p-3">
+	<p class="mb-7 text-background text-4xl font-title font-bold text-center">Appicole</p>
+	<!--<img src="/image1.png" alt="Smartphone" width="200" />-->
+	<p class="w-4/5 text-background my-5 text-left">Notre App qui vous permet de :</p>
+	<ul class="list-disc">
+		<li class="py-2 text-background">Consulter la carte de l'édition</li>
+		<li class="py-2 text-background">Vous conseiller la bière à déguster</li>
+		<li class="py-2 text-background">Sauvegarder & noter les bières que vous appréciez</li>
+		<li class="py-2 text-background">Participer à des activités exclusives</li>
+	</ul>
+    
+	<a target="_blank" href="https://app.brassicole.be" class="mt-10 mb-6">
+		<div
+			class="mx-auto p-4 bg-brass-orange font-title text-background font-semibold rounded-lg"
+		>
+			N'hésitez plus et téléchargez-la!
+		</div></a
+	>
 </div>
