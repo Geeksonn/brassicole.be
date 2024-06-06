6cd4cfde0806bb8bb2861d56e2dd3f556b3c9612
Olivier Sputael <sputael.o@gmail.com>
2024-06-06 17:11:19 +0200

    Added mockup app

--- src/lib/appInfo.svelte
+++ src/lib/appInfo.svelte
@@ -1,18 +1,15 @@
 <div class="h-full flex flex-col items-center bg-brass-green rounded-l-3xl p-3">
 	<p class="mb-7 text-background text-4xl font-title font-bold text-center">Appicole</p>
-	<!--<img src="/image1.png" alt="Smartphone" width="200" />-->
-	<p class="w-4/5 text-background my-5 text-left">Notre App qui vous permet de :</p>
-	<ul class="list-disc">
-		<li class="py-2 text-background">Consulter la carte de l'édition</li>
-		<li class="py-2 text-background">Vous conseiller la bière à déguster</li>
-		<li class="py-2 text-background">Sauvegarder & noter les bières que vous appréciez</li>
-		<li class="py-2 text-background">Participer à des activités exclusives</li>
-	</ul>
-    
+	<img src="/brassicole_mockup_cropped.png" alt="Smartphone" width="300" class="flex-grow" />
+
+	<div class="w-full flex justify-evenly items-center">
+		<p class="mt-3 p-3 text-background text-center text-sm">
+			Carte des bières, conseils de dégustation, notation des bières dégustées, activités exclusives
+		</p>
+	</div>
+
 	<a target="_blank" href="https://app.brassicole.be" class="mt-10 mb-6">
-		<div
-			class="mx-auto p-4 bg-brass-orange font-title text-background font-semibold rounded-lg"
-		>
+		<div class="mx-auto p-4 bg-brass-orange font-title text-background font-semibold rounded-lg">
 			N'hésitez plus et téléchargez-la!
 		</div></a
 	>
