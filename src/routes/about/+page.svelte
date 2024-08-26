748271a855713ab531ca56c46342fea0d2803b66
Olivier Sputael <sputael.o@gmail.com>
2024-08-26 13:37:22 +0200

    Added about section

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,19 @@
+<script>
+	import Header from '$lib/header.svelte';
+</script>
+
+<div class="flex flex-col min-h-screen">
+	<Header />
+    <div class="flex flex-col gap-y-4 mt-6 p-4">
+	<h1 class="font-title font-bold text-2xl text-brass-green">A propos de la Brassicole</h1>
+	<p>
+		La Brassicole est une organisation privée qui organise un événement annuel autour de la bière.
+		L'événement est privé et la vente d'alcool n'est autorisée qu'aux personnes majeures, comme le
+		stipule la loi belge applicable.
+	</p>
+	<p>
+		En marge de cet événément, La Brassicole a récemment développé une application mobile afin de
+		permettre aux visiteurs de consulter la carte, sauver & noter les bières qu'ils dégustent et
+		bien plus encore.
+	</p></div>
+</div>
