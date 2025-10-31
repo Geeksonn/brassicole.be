20889c534e6be54597b3c8cceaa0de64756c8e4c
Olivier Sputael <sputael.o@gmail.com>
2025-10-31 13:40:07 +0100

    changes before going to Nextjs

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,12 @@
 <script>
 	import EventDateCountdown from '$lib/eventDateCountdown.svelte';
-	import Header from '$lib/header.svelte';
 	import Footer from '$lib/footer.svelte';
-	import AppInfo from '$lib/appInfo.svelte';
-	import Meal from '$lib/meal.svelte';
 	import InfoCard from '$lib/infoCard.svelte';
 </script>
 
 <div class="flex flex-col gap-y-6 pt-6 sm:min-h-screen">
 	<div class="mx-auto">
-		<a href="/"> <img src="/brassicoleLogo.svg" alt="brassicole Logo" class="mx-auto" /></a>
+		<a href="/"> <img src="/brassicoleLogo.svg" alt="brassicole Logo" class="w-96" /></a>
 	</div>
 	<EventDateCountdown />
 	<div class="h-7 w-[1px] bg-brass-orange mx-auto"></div>
