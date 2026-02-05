359749d6074834f91896fb685c9559adfd1b6ab0
Olivier Sputael <sputael.o@gmail.com>
2026-02-05 21:03:07 +0100

    Fixed size of countdown element

--- src/lib/countdownElement.svelte
+++ src/lib/countdownElement.svelte
@@ -4,11 +4,11 @@
 </script>
 
 <div class="flex flex-col items-center">
-	<div class="px-2 w-20 sm:w-48 text-center">
-		<p class="font-title text-brass-pink text-3xl sm:text-9xl font-bold">
+	<div class="px-2 w-20 md:w-44 lg:w-56 xl:w-64 text-center">
+		<p class="font-title text-brass-pink text-3xl md:text-4xl lg:text-6xl xl:text-9xl font-bold">
 			{time.toString().padStart(2, '0')}
 		</p>
 	</div>
 
-	<p class="text-brass-green uppercase sm:text-xl text-xs tracking-[0.2em]">{text}</p>
+	<p class="text-brass-green uppercase md:text-xl text-xs tracking-[0.2em]">{text}</p>
 </div>
