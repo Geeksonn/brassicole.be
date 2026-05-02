edb1cb3364738eb204851cd481355177859885ac
geeksonn <sputael.o@gmail.com>
2026-05-02 15:09:54 +0200

    added app-support page

---
+++ src/routes/app-support/+page.svelte
@@ -0,0 +1,33 @@
+<script>
+	import Header from '$lib/header.svelte';
+</script>
+
+<div class="flex flex-col min-h-screen">
+	<Header />
+	<div class="flex flex-col gap-y-4 mt-6 p-4">
+		<h1 class="font-title font-bold text-2xl text-brass-green">
+			Appicole - Le compagnon idéal de l'événement
+		</h1>
+		<p>
+			L'appicole est désormais disponible sur vos app stores préférés ! Cette application mobile a
+			été conçue pour enrichir votre expérience lors de notre événement annuel. Avec l'appicole,
+			vous pouvez facilement consulter la carte des bières disponibles, sauvegarder vos préférées,
+			les noter et même recevoir des recommandations personnalisées pour votre prochaine
+			dégustation. Que vous soyez un amateur curieux ou un connaisseur passionné, l'appicole est
+			votre compagnon idéal pour tirer le meilleur parti de chaque édition de La Brassicole.
+		</p>
+		<p>
+			Si vous rencontrez des problèmes ou avez des questions concernant l'utilisation de l'appicole,
+			n'hésitez pas à nous contacter. Nous sommes là pour vous aider et nous assurer que votre
+			expérience avec notre application soit aussi agréable que possible. Vous pouvez nous envoyer
+			un e-mail à <a href="mailto:olivier@geekson.be">olivier@geekson.be</a>.
+		</p>
+		<h1 class="font-title font-bold text-2xl text-brass-green">Politique de Confidentialité</h1>
+		<p>
+			Consultez notre <a
+				href="https://app.brassicole.be/privacy-policy/"
+				class="underline text-brass-orange">politique de Confidentialité</a
+			>.
+		</p>
+	</div>
+</div>
